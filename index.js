b9e4e19827a6ef1085bfbff549447e24bd86b358
Zac Steinberg <zstein1234@gmail.com>
2019-07-31 19:42:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,9 +17,7 @@ function add2(n) {
 }
 
 var funkyFunction = function() {
-  return function() {
     return "FUNKY!";
-  };
 };
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
